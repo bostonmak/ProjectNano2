3861e87ed5b34b22af4eaf556bd7f54bf840d873
Boston Mak <boston@makotexas.com>
2017-10-02 21:53:49 -0700

    turn off exp coupon stacking

--- src/constants/ServerConstants.java
+++ src/constants/ServerConstants.java
@@ -90,7 +90,7 @@ public class ServerConstants {
     //Character Configuration
     public static final boolean USE_ADD_SLOTS_BY_LEVEL = true;  //Slots are added each 20 levels.
     public static final boolean USE_ADD_RATES_BY_LEVEL = true;  //Rates are added each 20 levels.
-    public static final boolean USE_STACK_COUPON_RATES = true;  //Multiple coupons effects builds up together.
+    public static final boolean USE_STACK_COUPON_RATES = false;  //Multiple coupons effects builds up together.
     public static final boolean USE_PERFECT_PITCH = true;	//For lvl 30 or above, each lvlup grants player 1 perfect pitch.
     public static final int FAME_GAIN_BY_QUEST = 5;             //Fame gain each N quest completes, set 0 to disable.
     public static final int SCROLL_CHANCE_RATE = 0;            //Number of rolls for success on a scroll, set 0 for default.
