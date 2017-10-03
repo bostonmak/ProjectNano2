44a75df597899477e552143aeaa8d3b70b79b1d6
Jay <deathnotezx@gmail.com>
2017-10-03 00:28:59 -0500

    Jank Wedding System added

--- scripts/npc/9201000.js
+++ scripts/npc/9201000.js
@@ -1,17 +1,17 @@
 function start() { 
-    cm.sendSimple("Which would you like? \r\n #L0# Moonstone Engagement Ring Box #l \r\n #L1# Star gem Engagement Ring Box #l \r\n #L2# Golden Heart Engagement Ring Box #l\r\n #L3# Silver Swan Engagement Ring Box #l"); 
+    cm.sendSimple("Which would you like? \r\n #L0# Moonstone Engagement Ring #l \r\n #L1# Star gem Engagement Ring #l \r\n #L2# Golden Heart Engagement Ring #l\r\n #L3# Silver Swan Engagement Ring #l"); 
 } 
 
 function action(mode, type, selection) { 
     if (mode == 1) { 
         if (selection == 0) 
-            cm.gainItem(2240000, 1); 
+            cm.gainItem(4031358, 1); 
         else if (selection == 1) 
-            cm.gainItem(2240001, 1); 
+            cm.gainItem(4031360, 1); 
         else if (selection == 2) 
-            cm.gainItem(2240002, 1); 
+            cm.gainItem(4031362, 1); 
 		 else if (selection == 3) 
-            cm.gainItem(2240002, 1); 
+            cm.gainItem(4031364, 1); 
 		//snow flake hat 1003755
     } 
     cm.dispose(); 
--- scripts/npc/9201002.js
+++ scripts/npc/9201002.js
@@ -1,52 +1,26 @@
-/*
-	This file is part of the OdinMS Maple Story Server
-    Copyright (C) 2008 Patrick Huy <patrick.huy@frz.cc>
-		       Matthias Butz <matze@odinms.de>
-		       Jan Christian Meyer <vimes@odinms.de>
+function start() { 
+    cm.sendSimple("Congratulations on your marriage! Please choose the ring that corresponds to your engagement ring! \r\n #L0# Moonstone Wedding Ring #l \r\n #L1# Star gem Wedding Ring #l \r\n #L2# Golden Heart Wedding Ring #l\r\n #L3# Silver Swan Wedding Ring #l"); 
+} 
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation version 3 as published by
-    the Free Software Foundation. You may not use, modify or distribute
-    this program under any other version of the GNU Affero General Public
-    License.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-/* NPC Base
-	Map Name (Map ID)
-	Extra NPC info.
- */
-
-var status;
- 
-function start() {
-        status = -1;
-        action(1, 0, 0);
-}
-
-function action(mode, type, selection) {
-        if (mode == -1) {
-                cm.dispose();
-        } else {
-                if (mode == 0 && type > 0) {
-                        cm.dispose();
-                        return;
-                }
-                if (mode == 1)
-                        status++;
-                else
-                        status--;
-    
-                if(status == 0) {
-                        cm.sendOk("Wedding is currently closed.");
-                        cm.dispose();
-                }
-        }
-}
+function action(mode, type, selection) { 
+    if (mode == 1) { 
+        if (selection == 0) 
+            cm.gainItem(4031358, -1)
+		    cm.gainItem(1112803, 1)
+			; 
+        else if (selection == 1) 
+            cm.gainItem(4031360, -1)
+		    cm.gainItem(1112806, 1)
+		; 
+        else if (selection == 2) 
+            cm.gainItem(4031362, -1)
+			cm.gainItem(1112807, 1)
+			;
+		 else if (selection == 3) 
+            cm.gainItem(4031364, -1) 
+			cm.gainItem(1112809, 1)
+		;
+		//snow flake hat 1003755
+    } 
+    cm.dispose(); 
+}  
