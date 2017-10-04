5a7d2c1dd052a881dac77f38321ed4cde87d4a12
Boston Mak <boston@makotexas.com>
2017-10-03 21:37:40 -0700

    jay stuff

--- scripts/npc/9201002.js
+++ scripts/npc/9201002.js
@@ -4,22 +4,22 @@ function start() {
 
 function action(mode, type, selection) { 
     if (mode == 1) { 
-        if (selection == 0) 
-            cm.gainItem(4031358, -1)
-		    cm.gainItem(1112803, 1)
-			; 
-        else if (selection == 1) 
+        if (selection == 0) {
+            cm.gainItem(4031358, -1);
+		    cm.gainItem(1112803, 1);
+        }
+        else if (selection == 1) {
             cm.gainItem(4031360, -1)
 		    cm.gainItem(1112806, 1)
-		; 
-        else if (selection == 2) 
+        }
+        else if (selection == 2) {
             cm.gainItem(4031362, -1)
 			cm.gainItem(1112807, 1)
-			;
-		 else if (selection == 3) 
+        }
+		 else if (selection == 3) {
             cm.gainItem(4031364, -1) 
 			cm.gainItem(1112809, 1)
-		;
+        }
 		//snow flake hat 1003755
     } 
     cm.dispose(); 
