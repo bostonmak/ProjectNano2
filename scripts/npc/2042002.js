d6ca7800f7418aec69b9273e5d38a46a543223be
Boston Mak <boston@makotexas.com>
2018-05-09 19:53:40 -0700

    Revert "rebirth npc edit and HH xml fix again"
    This reverts commit 3f3b7b509afc105196328ce2c136e7d7bd9b369d.

--- scripts/npc/2042002.js
+++ scripts/npc/2042002.js
@@ -32,7 +32,7 @@ function action(mode, type, selection) {
             status--;
         
                  if (status == 0) {
-        cm.sendYesNo("Welcome, great hero. You have been through a long and challenging road, and you have become immensely strong. If you bring me a Maple Coin,I can use my magic to increase your power even further, and surpass your limits! You will become a level 1 Beginner again, but you will keep your stats the same and all the Skill in you Hot Keys. Do you wish to be reborn? #b(Note: Please Make Sure you Have Room for all your Equipment in your Inventory. Also Make Sure you use all your SP. As it won't Be use able in your next Job. )#k" );
+        cm.sendYesNo("Welcome, great hero. You have been through a long and challenging road, and you have become immensely strong. If you bring me a Maple Coin,I can use my magic to increase your power even further, and surpass your limits! You will become a level 1 Beginner again, but you will keep your stats the same and all the Skill in you Hot Keys. Do you wish to be reborn? #b(Note: Please Make Sure you Have Room for all your Equipment in your Inventory. Also Make Sure you use all your SP. As it won't Be useable in your next Job. )#k" );
         }else if (status == 1) {
         if(cm.getPlayer().getLevel() < 200){
         cm.sendOk("Sorry, You have to be level 200 to rebirth.");
@@ -43,7 +43,7 @@ function action(mode, type, selection) {
          }else if (status == 2) {
         cm.getPlayer().setLevel(2);
         cm.changeJobById(0);
-		//cm.resetStats();
+		cm.resetStats();
         cm.sendNext("Enjoy your rebirth!(Note: You will have to Change Channels For the Full Effect to Take Place.PLEASE MAKE SURE TO SET YOUR AP BEFORE LEVELING USING any of the following @luk @int @str @dex.");
         cm.dispose();
         }            
